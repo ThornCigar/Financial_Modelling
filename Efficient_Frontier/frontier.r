8423cb13a80c993172af40524808df6ba514ce9d
ThornCigar <82872879+ThornCigar@users.noreply.github.com>
2022-09-25 23:10:18 +0800

    Basic sliding window implementation
    Also functionised codes, and corrected a major error

--- Efficient_Frontier/frontier.r
+++ Efficient_Frontier/frontier.r
@@ -2,16 +2,12 @@
 #
 # https://github.com/ThornCigar
 #
-
-library(dplyr)
-
-
-# Clear the environment
-rm(list = ls())
+# Special thanks to DragonXi for their contribution.
 
 
-# Import data
-data = read.csv("all_stocks_5yr.csv")
+library(dplyr) 
+rm(list = ls()) # Clear the environment
+data = read.csv("all_stocks_5yr.csv") # Import data
 
 
 # normalise_data(data) -> data
@@ -25,26 +21,16 @@ normalise_data = function(data) {
   data_count_target = data_count_count[order(-data_count_count$nn), ][1, "n"]
   purge_list = data_count[data_count$n != data_count_target, ]
   data = subset(data, !Name %in% purge_list$Name)
-
+  
   ## Remove data with N/A entries
   na_row = unique(which(is.na(data), arr.ind = TRUE)[, 1])
   data = data[!data$Name %in% data[na_row, ]$Name, ]
-
+  
   ## Add "mid" column
   data$mid = (data$open + data$close) / 2
-
+  
   return(data)
 }
-
-
-data = normalise_data(data)
-data_dim = count(data, Name) # unreliable stock name order
-data_col_count = nrow(data_dim)
-data_row_count = data_dim[1, 'n']
-
-
-# Reshape 1d price vector into 2d price matrix
-price_mat = matrix(data[["mid"]], nrow = data_row_count)
 
 
 # get_return_matrix(price_mat) -> return_mat
@@ -62,55 +48,88 @@ get_return_matrix = function(price_mat) {
 }
 
 
-return_mat = get_return_matrix(price_mat)
-return_mean = colMeans(return_mat)
-return_cov = cov(return_mat)
-
-
-
-A1 = cbind(return_cov, return_mean, rep(1, data_col_count))
-A2 = cbind(t(return_mean), 0, 0)
-A3 = cbind(t(rep(1, data_col_count)), 0, 0)
-A = rbind(A1, A2, A3)
-A_inv = solve(A)
-rm(A1, A2, A3)
-
-
-# Calculate minimum variance portfolio for each return
-# Formula is derived by solving the matrix form Lagrangian
-store = matrix(nrow = 1000, ncol = data_col_count + 2)
-# colnames(store) = c("Weight 1", "Return", "SD")
-for (i in seq(1, 1000)) {
-  target = 0.0001 + (i - 1) * 0.00001
-  b = c(rep(0, data_col_count), target, 1)
-  x = A_inv %*% b
-  # print(sum(x[1:data_col_count]))
-  var = t(x[1:data_col_count]) %*% return_cov %*% x[1:data_col_count]
-  store[i, 1:data_col_count] = x[1:data_col_count]
-  store[i, data_col_count + 1] = target
-  store[i, data_col_count + 2] = sqrt(var)
+# portfolios(return) -> store
+#
+# Use return matrix to calculate efficient portfolios, store weights, return and sd
+portfolios = function(return){
+  return_mean = colMeans(return)
+  return_cov = cov(return)
+  n = length(return_mean)
+  A1 = cbind(2 * return_cov, return_mean, rep(1, n))
+  A2 = cbind(t(return_mean), 0, 0)
+  A3 = cbind(t(rep(1, n)), 0, 0)
+  A = rbind(A1, A2, A3)
+  A_inv = solve(A)
+  rm(A1, A2, A3)
+  
+  # Calculate minimum variance portfolio for each return
+  # Formula is derived by solving the matrix form Lagrangian
+  store = matrix(nrow = 1000, ncol = n + 2)
+  # colnames(store) = c("Weight 1", "Return", "SD")
+  for (i in seq(1, 1000)) {
+    target = 0.0001 + (i - 1) * 0.00001
+    b = c(rep(0, n), target, 1)
+    x = A_inv %*% b
+    var = t(x[1:n]) %*% return_cov %*% x[1:n]
+    store[i, 1:n] = x[1:n]
+    store[i, n + 1] = target
+    store[i, n + 2] = sqrt(var)
+  }
+  return(store)
 }
 
-# Plot
+
+# Looking to functionise everything and hopefully remove this part later
+data = normalise_data(data)
+data_dim = count(data, Name) # unreliable stock name order
+data_col_count = nrow(data_dim)
+data_row_count = data_dim[1, 'n']
+
+
+# Reshape 1d price vector into 2d price matrix
+price_mat = matrix(data[["mid"]], nrow = data_row_count)
+return_mat = get_return_matrix(price_mat)
+
+
+# Calculate portfolios and plot
+store = portfolios(return_mat)
 plot(store[, data_col_count + 2], store[, data_col_count + 1], xlab = "SD", ylab = "Return")
 
 
-# Testing linear combination of efficient portfolios
-portfolio_1 = store[100, ]
-portfolio_2 = store[nrow(store) - 100, ]
-store2 = matrix(nrow = 10001, ncol = 2)
-j = 1
-for (i in seq(0, 1, 0.0001)) {
-  weight = i * portfolio_1[1:data_col_count] + (1 - i) * portfolio_2[1:data_col_count]
-  store2[j, 1] = t(weight) %*% return_mean
-  var = t(weight) %*% return_cov %*% weight
-  store2[j, 2] = sqrt(var)
-  j = j + 1
+# sliding(return, window, expand) -> store (3 dimensional)
+#
+# Sliding window implementation, singularity issue for small window to be fixed
+sliding = function(return, window=502, expand=FALSE){
+  n = ncol(return)
+  store = array(dim=c(1000,n + 2, nrow(return) - window + 1))
+  for (t in seq(1, nrow(return) - window + 1)) {
+    return_slice = return[t:(t + window - 1), ]
+    store[,,t] = portfolios(return_slice)
+  }
+  return(store)
 }
 
-plot(store2[, 2],
-     store2[, 1],
-     xlab = "SD",
-     ylab = "Return",
-     col = "blue")
-lines(store[, data_col_count + 2], store[, data_col_count + 1], col = "red")
+
+test = sliding(return_mat)
+
+
+## This part need to be re-written, but with very low priority
+# Testing linear combination of efficient portfolios
+# portfolio_1 = store[100, ]
+# portfolio_2 = store[nrow(store) - 100, ]
+# store2 = matrix(nrow = 10001, ncol = 2)
+# j = 1
+# for (i in seq(0, 1, 0.0001)) {
+#   weight = i * portfolio_1[1:data_col_count] + (1 - i) * portfolio_2[1:data_col_count]
+#   store2[j, 1] = t(weight) %*% return_mean
+#   var = t(weight) %*% return_cov %*% weight
+#   store2[j, 2] = sqrt(var)
+#   j = j + 1
+# }
+# 
+# plot(store2[, 2],
+#      store2[, 1],
+#      xlab = "SD",
+#      ylab = "Return",
+#      col = "blue")
+# lines(store[, data_col_count + 2], store[, data_col_count + 1], col = "red")
